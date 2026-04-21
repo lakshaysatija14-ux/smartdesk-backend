7cdfd006a95a29352d022ed28712614645b6fc96
Lakha Singh <lakhanamdhari2005@gmail.com>
2026-04-21 18:15:27 +0530

    fix:error

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const MONGO_URI="mongodb+srv://lakhanamdhari2005_db_user:6pqjJHsTQ4kmdZD7@cluster0.tpskgwv.mongodb.net/smartdesk?appName=Cluster0"
+
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{});
+        await mongoose.connect(MONGO_URI,{});
         console.log('MongoDB Connected');
     } catch (error) {
         console.error(error.message);
