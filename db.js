703f1fdf8a9a44a12af56e983e3893df6e280f57
lakshaysatija14-ux <lakshaysatija14@gmail.com>
2026-05-23 18:55:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI="mongodb+srv://lakhanamdhari2005_db_user:6pqjJHsTQ4kmdZD7@cluster0.tpskgwv.mongodb.net/smartdesk?appName=Cluster0"
+const MONGO_URI="mongodb+srv://lakshaysatija14_db_user:Lakshay123@cluster0.cjwqarp.mongodb.net/smartdesk?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDB = async() => {
     try {
